451991e0d4cc65d40f7f49c412951daf2c6d6cfc
Manan Kuraware <manankuraware2017@gmail.com>
2025-07-24 00:09:39 +0530

    ReactRouter

--- react_router/src/api/GetApiData.jsx
+++ react_router/src/api/GetApiData.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export const getMoviesData = async () => {
   try {
+    // USING ENV VARIABLE TO SECURE API
     const response = await fetch(
-      "http://www.omdbapi.com/?i=tt3896198&apikey=6186aa99"
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${
+        import.meta.env.VITE_API_KEY
+      }`
     );
     const data = response.json();
     return data;
