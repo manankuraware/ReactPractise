78b667d6b36b108f102b8d6c646da317ed6f3e17
Manan Kuraware <manankuraware2017@gmail.com>
2025-07-26 23:30:18 +0530

    Axios get

---
+++ axios-crud-operations/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import { getPost } from "./api/PostApi";
+
+const App = () => {
+  const getPostData = async () => {
+    const res = await getPost();
+    console.log(res.data);
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+  return <>CRUD operations</>;
+};
+
+export default App;
